f96d9d8c239f983a4a89311149cbec01398c915c
Sanidhya Mahna <sanidhya.mahna@gmail.com>
2022-10-30 18:15:19 +0530

    importing some filed

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -12,7 +12,7 @@ import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
-import insta from "@assets/instagram.jpeg";
+import insta from "../assets/Instagram.jpeg";
 import Image from "next/image";
 import HomeIcon from "@mui/icons-material/Home";
 import ExploreIcon from "@mui/icons-material/Explore";
--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import './signup/signup.css'
 import './login/login.css'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import '@components/Feed.css'
+import '../Components/Feed.css'
 import '../Components/Profile.css'
 import AuthWrapper from '../context/auth';
 function MyApp({ Component, pageProps }) {
